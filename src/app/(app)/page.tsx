d936bf9932ee0ee8d85aa70fa830292a393fc584
amitsaini144 <1amit24saini5@gmail.com>
2024-05-27 19:07:13 +0530

    Revert "updated homepage"

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,7 +4,6 @@ import { Mail } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Autoplay from 'embla-carousel-autoplay';
 import messages from '@/messages.json';
-import { GlobeDemo } from '@/components/Globe';
 
 import {
     Carousel,
@@ -15,8 +14,7 @@ import {
 export default function Home() {
     return (
         <>
-            <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 pt-12">
-                
+            <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 h-screen max-h-full">
                 <section className="text-center mb-8 md:mb-12">
                     <h1 className="text-3xl md:text-5xl font-bold">
                          World of Anonymous Feedback
@@ -52,7 +50,6 @@ export default function Home() {
                         ))}
                     </CarouselContent>
                 </Carousel>
-                <GlobeDemo />
             </main>
 
         </>
