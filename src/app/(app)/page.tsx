563de96e83915bfab23b155d749c0ce0b86c0933
amitsaini144 <1amit24saini5@gmail.com>
2024-05-30 02:21:50 +0530

    updated homepage heading ui

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,6 +4,7 @@ import { Mail } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Autoplay from 'embla-carousel-autoplay';
 import messages from '@/messages.json';
+import GradualSpacing from "@/components/magicui/gradual-spacing";
 
 import {
     Carousel,
@@ -15,16 +16,16 @@ export default function Home() {
     return (
         <>
             <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 h-screen max-h-full">
-                <section className="text-center mb-8 md:mb-12">
-                    <h1 className="text-3xl md:text-5xl font-bold">
-                         World of Anonymous Feedback
-                    </h1>
+                <section className="text-center mb-08 md:mb-12">
+                    <GradualSpacing
+                        className="font-display text-center text-4xl font-bold tracking-[-0.1em]  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+                        text="Anonymous Feedback"
+                    />
                     <p className="mt-3 md:mt-4 text-base md:text-lg">
                         SomeThink - Where your identity remains a secret.
                     </p>
                 </section>
 
-                {/* Carousel for Messages */}
                 <Carousel
                     plugins={[Autoplay({ delay: 3000 })]}
                     className="w-full max-w-lg md:max-w-xl"
