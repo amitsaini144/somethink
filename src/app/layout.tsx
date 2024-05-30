b6e9323ee690c43d6c7bc9e322cf43849eae08ba
amitsaini144 <1amit24saini5@gmail.com>
2024-05-30 19:22:55 +0530

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Toaster />
-            {/* <Footer /> */}
+            <Footer />
           </ThemeProvider>
         </body>
       </AuthProvider>
