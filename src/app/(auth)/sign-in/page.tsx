efc7de5cd48d4616ba614cb8fe88367536d0b598
amitsaini144 <1amit24saini5@gmail.com>
2024-05-30 20:27:30 +0530

    added relative to signin and signup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -70,7 +70,7 @@ export default function SignInForm() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen dark:bg-black/0">
+    <div className="flex justify-center items-center min-h-screen dark:bg-black/0 relative">
       <ShineBorder className="w-full max-w-sm md:max-w-md md:p-8 px-8 py-5 space-y-8 bg-white dark:bg-black/0 rounded-lg shadow-lg border" color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}>
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight mb-6">
