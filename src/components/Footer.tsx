00a54bfa8864a6749bd00331df7973be8f0e701f
amitsaini144 <1amit24saini5@gmail.com>
2024-05-30 19:00:23 +0530

    increased padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 function Footer() {
     return (
-        <footer className="text-center p-4 border-t border-red-400 z-50 dark:bg-black/0">
+        <footer className="text-center p-2 border-t border-red-400 dark:bg-black/0">
             © 2024 SomeThink. All rights reserved.
         </footer>
     )
