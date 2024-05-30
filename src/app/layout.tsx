068302fd19633ea52593764d267f64bb841ec4a7
amitsaini144 <1amit24saini5@gmail.com>
2024-05-30 18:45:06 +0530

    testing footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
           >
             <Navbar />
             {children}
-            <Footer />
             <Toaster />
+            <Footer />
           </ThemeProvider>
         </body>
       </AuthProvider>
